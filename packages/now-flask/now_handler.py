7f85ebd19f1d24010f3e5ad72e76b02462c2c68e
Burkay Gur <burkay.gur@coinbase.com>
2018-12-29 13:13:51 -0800

    First try at flask builder

---
+++ packages/now-flask/now_handler.py
@@ -0,0 +1,24 @@
+from http.server import HTTPServer
+import json
+import requests
+from __NOW_HANDLER_FILENAME import handler
+import _thread
+
+from wsgiref.simple_server import make_server
+server = make_server('0.0.0.0', 3000, app)
+server.set_app(app)
+
+def now_handler(event, context):
+    _thread.start_new_thread(server.handle_request, ())
+    payload = json.loads(event['body'])
+    path = payload['path']
+    headers = payload['headers']
+    method = payload['method']
+    
+    res = requests.request(method, 'http://0.0.0.0:3000' + path, headers=headers)
+
+    return {
+        'statusCode': res.status_code,
+        'headers': dict(res.headers),
+        'body': res.text
+    }
